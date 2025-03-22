8525756998adb462e5c04ab858af2163750bbff9
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-23 02:23:17 +0530

    new chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,18 @@ const SignIn = async () => {
   // if (session?.user) redirect("/dashboard");
 
   return (
-    <div className="flex min-h-screen w-full items-center justify-center flex-row bg-white">
-      <Card className="w-full md:w-[800px] md:rounded-3xl border-t-2 border-b-2 border-black flex items-center flex-row">
+    <div className="flex h-[100vh]  w-full items-center justify-center flex-col md:flex-row bg-white px-4">
+      <Card className="w-[100%] md:w-[800px] md:rounded-3xl border-t-2 border-b-2 border-black flex items-center flex-col md:flex-row ">
         <div
-          className="w-[900px] h-[450px] z-50"
+          className=" w-[400px] h-[400px] md:w-[900px] md:h-[450px] z-50"
           style={{
             backgroundImage: `url("/login.jpg")`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         ></div>
-        <CardBody className="h-screen gap-7 px-5 py-10 md:h-max md:p-14 md:shadow-2xl">
-          <h1 className="text-3xl font-light">Admin Login..!</h1>
+        <CardBody className=" gap-7 px-5 py-10 md:h-max md:p-14 md:shadow-2xl">
+          <h1 className="text-3xl font-light md:text-start text-center">Admin Login..!</h1>
           <div className="flex flex-col gap-5">
             <AuthForm />
           </div>
