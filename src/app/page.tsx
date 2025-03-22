f48c474bc2cbe1f25a73bbdfa836bbd3282a2f6f
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-23 02:11:17 +0530

    new chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,25 @@ import AuthForm from "@/components/forms/auth-form";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { Card, CardBody } from "@nextui-org/react";
+import { url } from "inspector";
 
 const SignIn = async () => {
   // const session = await getServerSession(authOptions);
   // if (session?.user) redirect("/dashboard");
 
   return (
-    <div className="flex min-h-screen w-full items-start justify-center md:py-20">
-      <Card className="w-full sm:w-[400px] md:rounded-3xl">
+    <div className="flex min-h-screen w-full items-center justify-center flex-row bg-white">
+      <Card className="w-full md:w-[800px] md:rounded-3xl border-t-2 border-b-2 border-black flex items-center flex-row">
+        <div
+          className="w-[900px] h-[450px] z-50"
+          style={{
+            backgroundImage: `url("/login.jpg")`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
         <CardBody className="h-screen gap-7 px-5 py-10 md:h-max md:p-14 md:shadow-2xl">
-          <h1 className="text-3xl font-light">Sign in as Admin </h1>
+          <h1 className="text-3xl font-light">Admin Login..!</h1>
           <div className="flex flex-col gap-5">
             <AuthForm />
           </div>
