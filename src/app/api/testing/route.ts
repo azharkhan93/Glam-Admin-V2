86ee6bee7f3f8382761d801994589667ba9a43a3
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-23 14:33:06 +0530

    new chnges

--- src/app/api/testing/route.ts
+++ src/app/api/testing/route.ts
@@ -1,6 +1,5 @@
 export async function GET(req: Request) {
-    return new Response(JSON.stringify({message: "hello Imam from test route"})
-    ,
+    return new Response(JSON.stringify({message: "hello Imam from test route"}),
     { status: 200}
 )
 }
