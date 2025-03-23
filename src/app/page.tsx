bef6b8a8c0ade723ac4959bd8bb6a9edda9a8321
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-23 13:09:43 +0530

    shadow error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const SignIn = async () => {
 
   return (
     <div className="flex h-[100vh]  w-full items-center justify-center flex-col md:flex-row bg-white px-4">
-      <Card className="w-[100%] md:w-[800px] md:rounded-3xl border-t-2 border-b-2 border-black flex items-center flex-col md:flex-row ">
+      <Card className="w-[100%] md:w-[800px] md:rounded-3xl border-t-2 border-b-2 border-black flex items-center flex-col md:flex-row  shadow-none md:shadow-lg">
         <div
           className=" w-full h-[360px] md:w-[900px] md:h-[450px] z-50"
           style={{
@@ -20,7 +20,7 @@ const SignIn = async () => {
             backgroundPosition: "center",
           }}
         ></div>
-        <CardBody className=" gap-7 px-5 py-10 md:h-max md:p-14 md:shadow-2xl">
+        <CardBody className="gap-7 px-5 py-10 md:h-max md:p-14 ">
           <h1 className="text-3xl font-light md:text-start text-center">Admin Login..!</h1>
           <div className="flex flex-col gap-5">
             <AuthForm />
