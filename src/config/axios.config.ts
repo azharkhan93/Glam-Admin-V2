66996bca19048d44651a81bc627bf598c30b80f1
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-23 13:23:35 +0530

    new changes

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://glam-admin-v2.vercel.app"
+    : "http://localhost:3001"; // Change to your local API URL
+
 export default axios.create({
-  baseURL: process.env.URL,
+  baseURL,
   // withCredentials: true,
 });
